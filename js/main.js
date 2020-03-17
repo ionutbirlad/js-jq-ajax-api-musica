c2a64dc23b1498d4689fe8f3db86e376eb56cfc6
Ionut Birlad <birladionut@yahoo.it>
2020-03-17 16:29:05 +0100

    sostituito il codice nel success con una funzione

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   var template = Handlebars.compile(source);
 // HANDLEBARS
 
-
+// Selezione genere musicale
   $("#selezione-genere").change(function () {
     var genereSelezionato = $("#selezione-genere").val().toLowerCase();
     // console.log(genereSelezionato);
@@ -20,8 +20,6 @@ $(document).ready(function() {
         }
       })
     }
-
-
   });
 
 
@@ -31,22 +29,7 @@ $(document).ready(function() {
     url: "https://flynn.boolean.careers/exercises/api/array/music",
     method: "GET",
     success: function (data) {
-      // console.log(data);
-      var albums = data.response;
-      // console.log(albums);
-      for (var i = 0; i < albums.length; i++) {
-        var album = {
-          image: albums[i].poster,
-          titolo: albums[i].title,
-          author: albums[i].author,
-          year: albums[i].year,
-          genere: albums[i].genre
-        };
-      // console.log(album);
-      var templateCompiled = template(album);
-      $(".cards").append(templateCompiled);
-
-      }
+      assegnazioneAlbum(data, ".cards");
     },
     error: function () {
       alert("Qualcosa è andato storto!");
@@ -55,6 +38,21 @@ $(document).ready(function() {
 
 
 
+// FUNZIONI
+  function assegnazioneAlbum(myData, placeToAppend) {
+    var variabileEstrapolata = myData.response;
+    for (var i = 0; i < variabileEstrapolata.length; i++) {
+      var elementoVariabileEstrapolata = {
+        image: variabileEstrapolata[i].poster,
+        titolo: variabileEstrapolata[i].title,
+        author: variabileEstrapolata[i].author,
+        year: variabileEstrapolata[i].year,
+        genere: variabileEstrapolata[i].genre
+      };
+    var templateCompiled = template(elementoVariabileEstrapolata);
+    $(placeToAppend).append(templateCompiled);
+    }
+  }
 
 
 
