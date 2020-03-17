de2a157761bbfe36cadc4ec410ce7bc9198af379
Ionut Birlad <birladionut@yahoo.it>
2020-03-17 14:59:54 +0100

    avanzamento

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
 
+
+  var source = $("entry-template").html();
+  var template = Handlebars.compile(source);
+
+
+  var templateCompiled = template(myObject);
+  $(".main.active").append(templateCompiled);
+
+
   $.ajax({
     url: "https://flynn.boolean.careers/exercises/api/array/music",
     method: "GET",
     success: function (data) {
-      console.log(data);
+      // console.log(data);
     },
     error: function () {
       alert("Qualcosa è andato storto!");
